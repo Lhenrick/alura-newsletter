f79d08c302a6aa004ba51ec65214afc66416bc31
Luan Henrique <152590653+Lhenrick@users.noreply.github.com>
2024-09-06 19:20:40 -0300

    adding initial files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+import ArticleList from "./assets/components/ArticleList"
+import Form from "./assets/components/Form"
+import Header from "./assets/components/Header"
+
+const App = () => {
+
+  const [user, setUser] = useState()
+  const hasUser = Boolean(user)
+  return (
+    <div className="h-screen">
+      <Header user={user}/>
+      { hasUser && <ArticleList />}
+      { hasUser || <Form onSubmit={setUser}/>}
+    </div>)
+}
+
+export default App
